1eedb02559584979634f10b9e32487ed4a6203a6
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-07-29 19:55:44 +0200

    Create co.develhope.basicOperations.java

---
+++ co.develhope.basicOperations.java
@@ -0,0 +1,8 @@
+public class Start {
+    public static void main(String[] args) {
+        int apples = 3;
+        int oranges = 4;
+        int fruitBasket = apples + oranges;
+        System.out.println("The sum is " + fruitBasket);
+    }
+}
