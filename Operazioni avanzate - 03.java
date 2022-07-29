f8d6e76d18b188bdd8515a5047a15100a0f917f0
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-07-29 20:35:35 +0200

    Create Operazioni avanzate - 03.java

---
+++ Operazioni avanzate - 03.java
@@ -0,0 +1,7 @@
+public class Start {
+    public static void main(String[] args) {
+        int apples = 13;
+        int users = 3;
+        System.out.println("The rest is " + (apples % users));
+    }
+}
