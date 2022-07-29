33e0729f2a289592e28f3c85b7e9b8b3a0281068
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-07-29 20:28:36 +0200

    Create Operazioni avanzate - 02.java

---
+++ Operazioni avanzate - 02.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main(String[] args) {
+        boolean doesItRain = true;
+        boolean isHot = true;
+        System.out.println("NOT " + (!doesItRain));
+        System.out.println("AND " + (doesItRain && isHot));
+        System.out.println("OR " + (doesItRain || isHot));
+
+    }
+}
