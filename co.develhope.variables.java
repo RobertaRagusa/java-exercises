711c279707cbe60cde54a373f01ccfbb170184ce
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-07-29 19:47:08 +0200

    Create co.develhope.variables.java

---
+++ co.develhope.variables.java
@@ -0,0 +1,8 @@
+public class Start {
+    public static void main(String[] args) {
+        byte apples = 3;
+        byte oranges = 4;
+        System.out.println("The apples are " + apples + " and the oranges are " + oranges);
+
+    }
+}
