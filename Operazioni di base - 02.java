1fa14b45a5e31507d9c08d00e5d6c4ad7f72f76e
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-07-29 20:05:59 +0200

    Create Operazioni di base - 02.java

---
+++ Operazioni di base - 02.java
@@ -0,0 +1,7 @@
+public class Start {
+    public static void main(String[] args) {
+        String name = "Lorenzo";
+        int age = 30;
+        System.out.println("The name is " + name + " and the age is " + age);
+    }
+}
