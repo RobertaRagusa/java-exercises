6abf3650b679468b5e52797fa293452b389740d9
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-07-30 11:51:48 +0200

    Create Operator if.java

---
+++ Operator if.java
@@ -0,0 +1,12 @@
+public class Start {
+    public static void main(String[] args) {
+        int age = 23;
+        if (age < 13) {
+            System.out.println("You are a baby");
+        } else if (age < 20) {
+            System.out.println("You are a teeneger");
+        }else {
+            System.out.println("You are an adult");
+        }
+    }
+}
