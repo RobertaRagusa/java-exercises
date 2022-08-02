e75ff494d142e0bf00d08813f28c81b7bbc1ed0b
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-08-02 16:52:19 +0200

    Create Esercizio - Operatore for.java

---
+++ Esercizio - Operatore for.java
@@ -0,0 +1,9 @@
+public class Start {
+    public static void main(String[] args) {
+        String myColleagueNames[] = new String[] {"Marco", "Alice", "Eugenio", "Paolo"};
+        System.out.println("My colleagues are " + myColleagueNames.length);
+        for (int i=0; i<myColleagueNames.length; i++) {
+            System.out.println("My colleague is " + myColleagueNames[i]);
+        }
+    }
+}
