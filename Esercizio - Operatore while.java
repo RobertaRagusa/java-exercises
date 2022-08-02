6b03789977755bd360cd307bd34f3bea8b5cfe1b
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-08-02 16:58:39 +0200

    Create Esercizio - Operatore while.java

---
+++ Esercizio - Operatore while.java
@@ -0,0 +1,9 @@
+public class Start {
+    public static void main(String[] args) {
+        double numberToDivide = 100;
+        while (numberToDivide > 2){
+            System.out.println("The number is " + numberToDivide);
+            numberToDivide = numberToDivide / 2;
+        }
+    }
+}
