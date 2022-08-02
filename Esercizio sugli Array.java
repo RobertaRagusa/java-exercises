39c811bba3a25f26209df3dc1fc616b86d110493
RobertaRagusa <97839179+RobertaRagusa@users.noreply.github.com>
2022-08-02 16:44:51 +0200

    Create Esercizio sugli Array.java

---
+++ Esercizio sugli Array.java
@@ -0,0 +1,9 @@
+public class Start {
+    public static void main(String[] args) {
+        String myColleagueNames[] = new String[] {"Marco", "Alice", "Eugenio", "Paolo"};
+        System.out.println("My 1 colleague is: " + myColleagueNames[0]);
+        System.out.println("My 2 colleague is: " + myColleagueNames[1]);
+        System.out.println("My 3 colleague is: " + myColleagueNames[2]);
+        System.out.println("My 4 colleague is: " + myColleagueNames[3]);
+    }
+}
